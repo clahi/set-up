09287b1a4a531933ec1def96e18635a0f6e23aaa
Abdala Mohamed Ahmed <45933962+clahi@users.noreply.github.com>
2025-02-21 16:25:31 +0300

    Update controller.sh

--- user_data/controller.sh
+++ user_data/controller.sh
@@ -48,7 +48,7 @@ sudo mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
+sudo kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
 
 kubectl get nodes
 
